8f39c8a941dd760fd93c3c6db4ed6050922ae207
iKolya01 <ezh_pro100_ezh@mail.ru>
2014-11-10 10:11:48 +0300

    ОПН
    Разбор строки и добавление в стэк

---
+++ WpfApplication2/WpfApplication2/RPN.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication2
+{
+    class RPN
+    {
+        public RPN() { }
+
+        private bool isANumber(string value)
+        {
+            string[] number = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (value == number[i])
+                    return true;
+            }
+
+            return false;
+        }
+
+        private string retEndDel(string returnString)
+        {
+            string result = "";
+
+            result = returnString[returnString.Length - 1].ToString();
+            returnString.Remove(returnString.Length - 1);
+
+            return result;
+        }
+
+        public string inPostfix(string IN)
+        {
+            string result = "";
+            string stack = "";
+
+            int length = IN.Length, i = 0;
+
+            while (i < length)
+            {
+                if (isANumber(IN[i].ToString()))
+                {
+                    result += IN[i];
+                }
+
+                switch (IN[i])
+                {
+                    case '(':
+                        {
+                            stack += '(';
+
+                            break;
+                        }
+                    case ')':
+                        {
+                            int j = stack.Length - 1;
+                            while (stack[j] != '(' && j >= 0)
+                            {
+                                result += retEndDel(stack);
+                                j--;
+                            }
+                            stack.Remove(stack.Length - 1);
+
+                            break;
+                        }
+                    case '+':
+                        {
+                            stack += "+";
+
+                            break;
+                        }
+                    case '-':
+                        {
+                            stack += "-";
+
+                            break;
+                        }
+                    case '*':
+                        {
+                            stack += "*";
+
+                            break;
+                        }
+                    case '/':
+                        {
+                            stack += "/";
+
+                            break;
+                        }
+                    case '!':
+                        {
+                            stack += "!";
+
+                            break;
+                        }
+                    case '=':
+                        {
+                            stack += "=";
+
+                            break;
+                        }
+                    case '<':
+                        {
+                            stack += "<";
+
+                            break;
+                        }
+                    case '>':
+                        {
+                            stack += ">";
+
+                            break;
+                        }
+                }
+
+
+            }
+
+            return result;
+        }
+    }
+}
